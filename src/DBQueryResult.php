4d6c22ad7b201035a9344e8e967cb4d1c42a3389
Ilya Averkov <ilya.averkov@gmail.com>
2024-03-30 15:54:02 +0400

    DB query result class + some other changes

---
+++ src/DBQueryResult.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Averkov\Cirno;
+
+abstract class DBQueryResult extends CirnoObject
+{
+	// Parent DB object
+	private $db = NULL;
+
+	public function __construct(Cirno $cirno, DB $db) {
+		$this->db = $config;
+		parent::__construct($cirno);
+	}
+
+	/**
+	 * Get a result row as an instance of $class_name
+	 * @param string $class_name class name
+	 */
+	public function fetchObject(string $class_name) {
+
+	}
+
+	public function fetchAssoc(): array|false {
+
+	}
+}
